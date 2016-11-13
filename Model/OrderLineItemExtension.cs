49fb46ed529d8e09fa07f08a292ca69a9efdd586
Jasmin Savard <jasmin.savard@gmail.com>
2016-11-13 14:28:10 -0500

    Removing unused dependency

--- Model/OrderLineItemExtension.cs
+++ Model/OrderLineItemExtension.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using VirtoCommerce.Domain.Order.Model;
-using VirtoCommerce.Domain.ProductConfiguration.Model;
 
 namespace VirtoCommerce.OrderExtModule.Web.Model {
     public class OrderLineItemExtension : LineItem {
